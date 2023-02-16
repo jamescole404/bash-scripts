9759c5a1623263c9d07e80843f2283bc651cd09d
James <59737949+jamescole404@users.noreply.github.com>
2023-02-16 16:52:58 -0500

    Update rhonly.sh

--- rhonly.sh
+++ rhonly.sh
@@ -4,10 +4,7 @@
 TIME=$(date "+%Y%m%d-%H%M%S")
 echo "Get updates. Please wait..." 
 echo $TIME
-#reposync -n --config=/home/xadministrator/redhat.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/rh/$TIME+redhat6.9.log
-#reposync -n --config=/home/xadministrator/redhat6.7.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/rh/$TIME+redhat6.7.log
-#reposync -n --config=/home/xadministrator/redhat6.4.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/rh/$TIME+redhat6.4.log
-#reposync -n --config=/home/xadministrator/redhat6.3.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/rh/$TIME+redhat6.3.log
+
 #reposync -n  --download_path=/var/repos/redhat/7 --downloadcomps --download-metadata > /var/reposlogs/rh/$TIME+redhat7.log
 
 echo "Checking for Thumbdrive."
