f2fd0ba21ce2d1b7d3a1233b8eec97d96ad09267
James <59737949+jamescole404@users.noreply.github.com>
2023-02-16 16:59:45 -0500

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 TIME=`date "+%Y%m%d-%H%M%S"`
-#rsync --log-file=/var/reposlogs/$TIME.mariadb.log -a --partial --delete-after  rsync.osuosl.org::mariadb /var/repos/mariadb
+#rsync --log-file=/var/relogs/$TIME.mariadb.log -a --partial --delete-after  rsync.osuosl.org::mariadb /var/re/mariadb
 
-reposync -n --config=/home/xadministrator/redhat6.10.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/$TIME+redhat6.10.log
+rsync -n --config=/home/user/redhat6.10.repo --download_path=/var/re/redhat/6 --downloadcomps --download-metadata > /var/relogs/$TIME+redhat6.10.log
 
-reposync -n --config=/home/xadministrator/redhat.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/$TIME+redhat6.9.log
+rsync -n --config=/home/user/redhat.repo --download_path=/var/re/redhat/6 --downloadcomps --download-metadata > /var/relogs/$TIME+redhat6.9.log
 
-reposync -n --config=/home/xadministrator/redhat6.7.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/$TIME+redhat6.7.log
+rsync -n --config=/home/user/redhat6.7.repo --download_path=/var/re/redhat/6 --downloadcomps --download-metadata > /var/relogs/$TIME+redhat6.7.log
 
-reposync -n --config=/home/xadministrator/redhat6.4.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/$TIME+redhat6.4.log
+rsync -n --config=/home/user/redhat6.4.repo --download_path=/var/re/redhat/6 --downloadcomps --download-metadata > /var/relogs/$TIME+redhat6.4.log
 
-reposync -n --config=/home/xadministrator/redhat6.3.repo --download_path=/var/repos/redhat/6 --downloadcomps --download-metadata > /var/reposlogs/$TIME+redhat6.3.log
+rsync -n --config=/home/user/redhat6.3.repo --download_path=/var/re/redhat/6 --downloadcomps --download-metadata > /var/relogs/$TIME+redhat6.3.log
 
-reposync -n  --download_path=/var/repos/redhat/7 --downloadcomps --download-metadata > /var/reposlogs/$TIME+redhat7.log
+rsync -n  --download_path=/var/re/redhat/7 --downloadcomps --download-metadata > /var/relogs/$TIME+redhat7.log
 
